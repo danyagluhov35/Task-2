d4b5852ad7a8ce9f0f5434e0c0130010942edf84
DIVGP <76007580+danyagluhov35@users.noreply.github.com>
2025-03-05 12:51:15 +0300

    Update AuthOption.cs

--- TestTask/Core/AuthOption.cs
+++ TestTask/Core/AuthOption.cs
@@ -8,13 +8,14 @@ namespace TestTask.Core
     /// </summary>
     public class AuthOption
     {
-        // Секретный ключ для подписи и верификации JWT токенов.
+        // Секретный ключ для подписи и верификации JWT токенов. 
+        // В реальных приложениях лучше использовать конфиг файлы для хранения ключа, например в appsettings.json, т.к хранения ключа в коде небезопасно
         private const string Key = "1b>Z6kh~+O9[L3ic>>GCNxZ[X>B.71,])xJV2!rM?=8dwJj[W3(K~I,8=*8Cg/i5~TF+C\\L+b9r?_8Ey/o2#Zk$?Fa)M:<+JY~!w";
 
         // Издатель токена (Issuer) - уникальный идентификатор сервиса или приложения, которое генерирует токены.
         public static string Issuer = "TestTask";
 
-        // Аудитория токена (Audience) - это то, для какого приложения или пользователей предназначен токен.
+        // Для какого приложения или пользователей предназначен токен.
         public static string Audience = "UserTestTask";
 
         /// <summary>
